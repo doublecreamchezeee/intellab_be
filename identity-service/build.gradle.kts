217051e51425ee8e697f05a361f79a3606c081bf
21120575 <nguyen.thanhtri2112@gmail.com>
2024-11-26 18:27:09 +0700

    config docker in identity-service

--- identity-service/build.gradle.kts
+++ identity-service/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("org.springframework.boot") version "3.3.5"
-    id("io.spring.dependency-management")
+    id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.0"
+}
+
+repositories {
+    mavenCentral()
 }
 
 java {
@@ -16,11 +20,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.google.firebase:firebase-admin:8.1.0")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-//    implementation("org.springframework.security:spring-security-crypto")
-//    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("com.nimbusds:nimbus-jose-jwt:9.15.2")
     implementation("org.apache.commons:commons-lang3:3.12.0")
     compileOnly("org.projectlombok:lombok")
@@ -28,11 +29,9 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-//    runtimeOnly("org.postgresql:postgresql")
-//    runtimeOnly("com.h2database:h2")
+
+    // Spring Boot test dependency brings in JUnit 5
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
@@ -42,5 +41,5 @@ dependencyManagement {
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+    useJUnitPlatform()  // Ensures JUnit 5 is used for tests
 }
