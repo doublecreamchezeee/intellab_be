34712badb5f3f26dc243cfe2937bc0844ba6baed
21120575 <nguyen.thanhtri2112@gmail.com>
2024-12-01 17:59:10 +0700

    fix: handle cors error

--- api-gateway/src/main/java/com/example/apigateway/configuration/ClientConfiguration.java
+++ api-gateway/src/main/java/com/example/apigateway/configuration/ClientConfiguration.java
@@ -3,9 +3,14 @@ package com.example.apigateway.configuration;
 import com.example.apigateway.client.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 public class ClientConfiguration {
@@ -18,6 +23,32 @@ public class ClientConfiguration {
         return WebClient.builder()
                 .baseUrl(baseUrl)
                 .build();
+    }
+
+//    @Bean
+    CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        String hostname = DotenvConfig.get("HOST_NAME");
+        String identityPort = DotenvConfig.get("DOCKER_IDENTITY_PORT");
+        String coursePort = DotenvConfig.get("DOCKER_COURSE_PORT");
+        String apiGatewayPort = DotenvConfig.get("DOCKER_API_GATEWAY_PORT");
+        String fePort = DotenvConfig.get("DOCKER_FRONTEND_PORT");
+
+        System.out.println(hostname + fePort);
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://" + hostname + ":" + identityPort,
+                "http://" + hostname + ":" + coursePort,
+                "http://" + hostname + ":" + apiGatewayPort,
+                "http://" + hostname + ":" + fePort
+        ));
+//        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
