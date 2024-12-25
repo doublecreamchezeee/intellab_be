552b87147f2e2f11682e9c2baa91c10b15cc9a71
Phạm Đình Tiến <136001495+TienPham569us@users.noreply.github.com>
2024-12-25 20:28:13 +0700

    dev: crud review

--- course-service/src/main/java/com/example/courseservice/mapper/ReviewMapper.java
+++ course-service/src/main/java/com/example/courseservice/mapper/ReviewMapper.java
@@ -1,6 +1,7 @@
 package com.example.courseservice.mapper;
 
 import com.example.courseservice.dto.request.review.ReviewCreationRequest;
+import com.example.courseservice.dto.response.rerview.DetailsReviewResponse;
 import com.example.courseservice.dto.response.rerview.ReviewCreationResponse;
 import com.example.courseservice.model.Review;
 import org.mapstruct.Mapper;
@@ -8,16 +9,24 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
-//    @Mapping(target = "review_id", ignore = true)
+    @Mapping(target = "reviewId", ignore = true)
     @Mapping(target = "rating", source = "rating")
     @Mapping(target = "comment", source = "comment")
-    @Mapping(target = "userUid", source = "userUid")
+   // @Mapping(target = "courseId", source = "courseId")
+    @Mapping(target = "userUid", ignore = true)
     Review toReview(ReviewCreationRequest request);
 
-    @Mapping(target = "review_id", source = "review_id")
+    @Mapping(target = "reviewId", source = "reviewId")
     @Mapping(target = "rating", source = "rating")
     @Mapping(target = "comment", source = "comment")
     @Mapping(target = "userUid", source = "userUid")
     @Mapping(target = "courseId", source = "course.courseId")
     ReviewCreationResponse toReviewCreationResponse(Review review);
+
+    @Mapping(target = "reviewId", source = "reviewId")
+    @Mapping(target = "rating", source = "rating")
+    @Mapping(target = "comment", source = "comment")
+    @Mapping(target = "userUid", source = "userUid")
+    @Mapping(target = "courseId", source = "course.courseId")
+    DetailsReviewResponse toDetailsReviewResponse(Review review);
 }
--- course-service/src/main/java/com/example/courseservice/repository/ReviewRepository.java
+++ course-service/src/main/java/com/example/courseservice/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.example.courseservice.repository;
 
 import com.example.courseservice.model.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
-
+    Page<Review> findAllByCourse_CourseId(UUID courseId, Pageable pageable);
 }
