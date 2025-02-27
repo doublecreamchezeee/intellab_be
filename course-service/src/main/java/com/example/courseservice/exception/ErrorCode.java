5789147c51c77eb48a876ea6e564d973d5a39afb
Phạm Đình Tiến <136001495+TienPham569us@users.noreply.github.com>
2025-02-28 01:39:38 +0700

    feat: add filter review

--- course-service/src/main/java/com/example/courseservice/exception/ErrorCode.java
+++ course-service/src/main/java/com/example/courseservice/exception/ErrorCode.java
@@ -37,6 +37,7 @@ public enum ErrorCode {
     COMMENT_NOT_FOUND(404, "Comment not found", HttpStatus.NOT_FOUND),
     INVALID_COMMENT(409, "Invalid comment", HttpStatus.CONFLICT),
     INVALID_USER(409, "Invalid user", HttpStatus.CONFLICT),
+    INVALID_RATING_VALUE_FILTER(409, "Invalid rating value filter", HttpStatus.CONFLICT),
     ;
     ErrorCode(int code, String message) {
         this.code = code;
--- course-service/src/main/java/com/example/courseservice/repository/ReviewRepository.java
+++ course-service/src/main/java/com/example/courseservice/repository/ReviewRepository.java
@@ -4,13 +4,14 @@ import com.example.courseservice.model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, UUID> {
+public interface ReviewRepository extends JpaRepository<Review, UUID>, JpaSpecificationExecutor<Review> {
     Page<Review> findAllByCourse_CourseId(UUID courseId, Pageable pageable);
     List<Review> findAllByCourse_CourseId(UUID courseId);
 }
