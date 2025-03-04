bad24a1b5cc33d5401a30ea92e066192c0130025
Phạm Đình Tiến <136001495+TienPham569us@users.noreply.github.com>
2025-03-05 00:27:59 +0700

    ops: init payment service

---
+++ payment-service/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web-services")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	implementation("io.github.cdimascio:dotenv-java:3.0.0")
+	implementation("org.hibernate:hibernate-core:6.2.10.Final")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("io.github.cdimascio:dotenv-java:2.2.4")
+}
+
+tasks.withType<BootJar>  {
+	enabled = true
+}
---
+++ payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.paymentservice;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
