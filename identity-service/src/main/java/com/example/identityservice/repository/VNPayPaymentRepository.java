69b11b00512ec425e8354011f149d3ed6440d507
Nguyễn Thành Trí <nguyen.thanhtri2112@gmail.com>
2025-05-18 11:17:44 +0700

    fix: overview

--- identity-service/src/main/java/com/example/identityservice/repository/VNPayPaymentRepository.java
+++ identity-service/src/main/java/com/example/identityservice/repository/VNPayPaymentRepository.java
@@ -25,16 +25,16 @@ public interface VNPayPaymentRepository
 
     Page<VNPayPayment> findAllByUserUuid(UUID userUuid, Pageable pageable);
 
-    @Query("SELECT COALESCE(SUM(p.paidAmount), 0) FROM VNPayPayment p WHERE p.transactionStatus = 'PAID'")
+    @Query("SELECT COALESCE(SUM(p.paidAmount), 0) FROM VNPayPayment p WHERE p.transactionStatus = 'Active'")
     Float sumSuccessfulPayments();
 
-    @Query("SELECT COALESCE(SUM(p.paidAmount), 0) FROM VNPayPayment p WHERE p.transactionStatus = 'PAID' AND MONTH(p.createdAt) = :month AND YEAR(p.createdAt) = :year")
+    @Query("SELECT COALESCE(SUM(p.paidAmount), 0) FROM VNPayPayment p WHERE p.transactionStatus = 'Active' AND MONTH(p.createdAt) = :month AND YEAR(p.createdAt) = :year")
     Float sumSuccessfulPaymentsByMonth(@Param("month") int month, @Param("year") int year);
 
     @Query("""
                 SELECT FUNCTION('DATE_TRUNC', :unit, p.createdAt) AS period, SUM(p.paidAmount)
                 FROM VNPayPayment p
-                WHERE p.transactionStatus = 'PAID' AND p.createdAt BETWEEN :start AND :end
+                WHERE p.transactionStatus = 'Active' AND p.createdAt BETWEEN :start AND :end
                 GROUP BY period
             """)
     List<Object[]> sumRevenueByRange(
